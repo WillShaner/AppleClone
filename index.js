657d9d4cbc995a225cfe63bf63a3df8dbe73e233
William Shaner <williamshaner3@gmail.com>
2022-11-14 13:27:54 -0600

    update of images, styling changes, image scroll carousel, island feature added, accessories section added

---
+++ index.js
@@ -0,0 +1,21 @@
+var container = document.querySelector('.image-wrapper');
+var boxes = document.querySelector('.image-holder');
+
+gsap.to(boxes, {
+  x: () => {
+    return -(container.scrollWidth - document.body.offsetWidth) + 'px';
+  },
+  ease: 'sine',
+  scrollTrigger: {
+    trigger: boxes,
+    start: () => 'end end',
+    end: () => {
+      return '+=' + container.scrollWidth;
+    },
+    scrub: true,
+    pin: document.querySelector('.container-fluid'),
+    anticipatePin: 1,
+  },
+});
+
+console.log(container.scrollWidth, document.body.offsetWidth);
